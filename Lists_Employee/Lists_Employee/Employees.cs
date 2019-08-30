3a1e41e2376a119e49b31e245f5b1a3070a414cb
Rafael Oliveira <rafanegresco@hotmail.com>
2019-08-30 01:38:36 -0300

    Versao 2.0

---
+++ Lists_Employee/Lists_Employee/Employees.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lists_Employee
+{
+    class Employees
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public void IncreaseSalary(double dIncrease)
+        {
+            Salary += Salary * (dIncrease / 100);
+        }
+
+        public override string ToString()
+        {
+            return "\nID: " + Id
+                + "\nName: " + Name
+                + "\nSalary: R$ " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- Lists_Employee/Lists_Employee/Program.cs
+++ Lists_Employee/Lists_Employee/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
 
 namespace Lists_Employee
 {
@@ -6,7 +8,64 @@ namespace Lists_Employee
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int iIdEmp = 0;
+            int iNumReg = 0;
+            int iIndex = 0;
+
+            string sNameEmp = "";
+
+            double dSalaryEmp = 0d;
+            double dIncrease = 0d;
+
+            List<Employees> emp = new List<Employees>();
+
+            Console.WriteLine("***** Employee Register *****");
+
+            Console.Write("How many employees will be registered? ");
+            iNumReg = int.Parse(Console.ReadLine());
+
+            if(iNumReg > 0)
+            {
+                for (int i = 0; i < iNumReg; i++)
+                {
+                    Console.Write("What employee's ID? ");
+                    iIdEmp = int.Parse(Console.ReadLine());
+
+                    Console.Write("What employee's Name? ");
+                    sNameEmp = Console.ReadLine();
+
+                    Console.Write("What employee's Salary? ");
+                    dSalaryEmp = double.Parse(Console.ReadLine());
+
+                    emp.Add(new Employees { Id = iIdEmp, Name = sNameEmp, Salary = dSalaryEmp });
+                }
+
+                Console.WriteLine();
+                Console.Write("Enter the employee ID that will have salary increase: ");
+                iIdEmp = int.Parse(Console.ReadLine());
+
+                iIndex = emp.FindIndex(x => x.Id == iIdEmp);
+
+                if (iIndex >= 0)
+                {
+                    Console.WriteLine();
+                    Console.Write("Enter the percentage: ");
+                    dIncrease = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                    emp[iIndex].IncreaseSalary(dIncrease);
+                }
+
+                Console.WriteLine("Updated list of employees:");
+
+                foreach (Employees item in emp)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Obrigado!");
+            }
         }
     }
 }
